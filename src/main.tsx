dd6c058ab3735bea37c5438c7d72a76d3851ab2e
Calvin Lee <clee421@gmail.com>
2025-06-19 11:06:18 -0400

    feat: add gsap to bounce card

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,51 @@
-import React, { ReactElement } from "react";
-import { Body2, Title1 } from "@fluentui/react-components";
+import React, { ReactElement, useRef } from "react";
+import { Title1, Card, Button } from "@fluentui/react-components";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 import "./main.scss";
 
 const Main = (): ReactElement => {
+  const scope = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({ defaults: { ease: "power3.out" } });
+
+      tl.from(".card", {
+        y: -80,
+        opacity: 0,
+        duration: 0.8,
+      }).from(
+        ".title",
+        {
+          x: -40,
+          opacity: 0,
+          duration: 0.6,
+        },
+        "-=0.4"
+      );
+    },
+    { scope }
+  );
+
+  const handleClick = () => {
+    gsap
+    .timeline({ repeat: 1, repeatDelay: 0.2 })
+    .to(".card", { y: -30, duration: 0.15, ease: "power3.out" })
+    .to(".card", { y: 0,  duration: 0.4,  ease: "bounce.out" });
+  };
+
   return (
-    <div className="main-container">
-      <div>
-        <Title1>calvin lee</Title1>
-      </div>
-      <div className="text">
-        <Body2>
+    <div ref={scope} className="main-container">
+      <Card className="card" style={{ width: 535, padding: 24 }} onClick={handleClick}>
+        <Title1 className="title">calvin lee</Title1>
+        <p style={{ marginTop: 12 }}>
           Welcome to my playground for frontend things. To see my personal site
           go to <a href="https://calvinlee.dev/">calvinlee.dev</a>
-        </Body2>
-      </div>
+        </p>
+      </Card>
     </div>
   );
 };
