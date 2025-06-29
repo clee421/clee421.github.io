ca3b9954455e97b7ef97a8d9569e477759cac9fc
Calvin Lee <clee421@gmail.com>
2025-06-28 23:01:04 -0400

    feat: different on click animations

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import SplitText from "gsap/SplitText";
 import "./main.scss";
 
 const Main = (): ReactElement => {
+  const isAnimating = useRef(false)
   const scope = useRef<HTMLDivElement>(null);
 
   useGSAP(
@@ -26,16 +27,47 @@ const Main = (): ReactElement => {
     { scope }
   );
 
-  const handleClick = () => {
+  const jump = (onComplete: () => void) => {
     gsap
-    .timeline()
-    .to(".card", { y: -30, duration: 0.15, ease: "power3.out" })
-    .to(".card", { y: 0,  duration: 0.4,  ease: "bounce.out" })
-    .to([".card-intro"], { y: 6, duration: 0.12, ease: "power3.out" }, "-=0.25")
-    .to([".card-intro"], { y: -10, duration: 0.12, ease: "power3.out" })
-    .to([".card-intro"], { y: 0, duration: 0.15, ease: "bounce.out"})
-    .to([".card-intro"], { y: -6, duration: 0.08, ease: "power3.out" })
-    .to([".card-intro"], { y: 0, duration: 0.12, ease: "bounce.out", stagger: 0.05 });
+      .timeline()
+      .to(".card", { y: -30, duration: 0.15, ease: "power3.out" })
+      .to(".card", { y: 0,  duration: 0.4,  ease: "bounce.out" })
+      .to([".card-intro"], { y: 6, duration: 0.12, ease: "power3.out" }, "-=0.25")
+      .to([".card-intro"], { y: -10, duration: 0.12, ease: "power3.out" })
+      .to([".card-intro"], { y: 0, duration: 0.15, ease: "bounce.out"})
+      .to([".card-intro"], { y: -6, duration: 0.08, ease: "power3.out" })
+      .to([".card-intro"], { y: 0, duration: 0.12, ease: "bounce.out", stagger: 0.05 })
+      .eventCallback("onComplete", onComplete);
+  }
+
+  const drop = (onComplete: () => void) => {
+    gsap
+      .timeline({ defaults: { ease: "power2.out" } })
+      .to(".card", { y: 50, autoAlpha: 0, duration: 0.3, ease: "power2.in" })
+      .set(".card", { y: -50 })
+      .to(".card", { y: 0, autoAlpha: 1, duration: 0.3 })
+      .to(".card-intro", { y: 25, autoAlpha: 0, duration: 0.3, ease: "power2.in" },  "-=0.25")
+      .set(".card-intro", { y: -25 })
+      .to(".card-intro", { y: 0, autoAlpha: 1, duration: 0.3 })
+      .eventCallback("onComplete", onComplete);
+  };
+
+  const spin = (onComplete: () => void) => {
+    gsap
+      .timeline()
+      .to(".card", { rotation: 360, duration: 0.45, ease: "power2.out" })
+      .to(".card", { rotation: 0,   duration: 0.45, ease: "power2.inOut" })
+      .eventCallback("onComplete", onComplete);
+  }
+
+  const handleClick = () => {
+    if (isAnimating.current) return;
+    isAnimating.current = true;
+
+    const actions = [jump, drop, spin];
+    const actionIndex = Math.floor(Math.random() * actions.length)
+    const action = actions[actionIndex]
+    action(() => { isAnimating.current = false })
   };
 
   return (
